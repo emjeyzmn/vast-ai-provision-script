1d4822d14ac61b6a83f6443ac641e8402da0ac6d
Emjey zamani <71161398+emjeyzmn@users.noreply.github.com>
2025-12-05 14:29:30 +0330

    Update upscale_script_installing.sh
    add codes

--- upscale_script_installing.sh
+++ upscale_script_installing.sh
@@ -1,1 +1,189 @@
+#!/bin/bash
+
+source /venv/main/bin/activate
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
+
+# ======================================================
+#   YOUR CUSTOM CONFIG
+# ======================================================
+
+APT_PACKAGES=(
+)
+
+PIP_PACKAGES=(
+)
+
+# -------------------------
+# CUSTOM NODES (Git Repos)
+# -------------------------
+NODES=(
+    "https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git"
+    "https://github.com/yolain/ComfyUI-Easy-Use.git"
+    "https://github.com/kijai/ComfyUI-KJNodes.git"
+    "https://github.com/kijai/ComfyUI-Florence2.git"
+    "https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git"
+    "https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git"
+    "https://github.com/cubiq/ComfyUI_essentials.git"
+    "https://github.com/ClownsharkBatwing/RES4LYF.git"
+)
+
+# -------------------------
+# MODEL DOWNLOADS
+# -------------------------
+
+CHECKPOINT_MODELS=(
+)
+
+UNET_MODELS=(
+    # Wan2.2 GGUF (To: Unet/controlnet)
+    "https://huggingface.co/bullerwins/Wan2.2-T2V-A14B-GGUF/resolve/main/wan2.2_t2v_low_noise_14B_Q8_0.gguf"
+)
+
+LORA_MODELS=(
+    # Wan2.2 Lightning Low Noise
+    "https://huggingface.co/lightx2v/Wan2.2-Lightning/resolve/main/Wan2.2-T2V-A14B-4steps-lora-250928/low_noise_model.safetensors"
+
+    # Civitai File (download via CIVITAI_TOKEN)
+    "https://civitai.com/api/download/models/2179627"
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+)
+
+ESRGAN_MODELS=(
+    "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/a86fc6182b4650b4459cb1ddcb0a0d1ec86bf3b0/RealESRGAN_x2.pth"
+)
+
+CONTROLNET_MODELS=(
+)
+
+TEXT_ENCODER_MODELS=(
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+)
+
+WORKFLOWS=(
+)
+
+# ======================================================
+#   DO NOT EDIT BELOW THIS LINE
+# ======================================================
+
+function provisioning_start() {
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/checkpoints" \
+        "${CHECKPOINT_MODELS[@]}"
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/Unet/controlnet" \
+        "${UNET_MODELS[@]}"
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/loras/controlnet" \
+        "${LORA_MODELS[@]}"
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/vae" \
+        "${VAE_MODELS[@]}"
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/esrgans" \
+        "${ESRGAN_MODELS[@]}"
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/text_encoders" \
+        "${TEXT_ENCODER_MODELS[@]}"
+
+    provisioning_print_end
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+        sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+        pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="${COMFYUI_DIR}/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                (cd "$path" && git pull)
+                if [[ -e $requirements ]]; then
+                    pip install --no-cache-dir -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                pip install --no-cache-dir -r "$requirements"
+            fi
+        fi
+    done
+}
+
+function provisioning_get_files() {
+    if [[ -z $2 ]]; then return 1; fi
+
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+
+    arr=("$@")
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Application will start now\n\n"
+}
+
+function provisioning_download() {
+    if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
+        auth_token="$HF_TOKEN"
+    elif [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
+        auth_token="$CIVITAI_TOKEN"
+    fi
+
+    if [[ -n $auth_token ]]; then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition \
+            --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    else
+        wget -qnc --content-disposition --show-progress \
+            -e dotbytes="${3:-4M}" -P "$2" "$1"
+    fi
+}
+
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
+fi
 
